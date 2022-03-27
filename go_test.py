639ac6e39f3abac1fd4fdd822172e370e6a539c9
Eddie Huang <aigagror@gmail.com>
2022-03-27 14:44:08 -0700

    Implemented more unit tests

--- go_test.py
+++ go_test.py
@@ -20,7 +20,9 @@ class GoTest(unittest.TestCase):
         self.assertEqual(state.shape, (2, 6, 4, 4))
 
     def test_to_indicator_actions_pass(self):
-        self.assertEqual(True, False)  # add assertion here
+        state = go.new_states(2)
+        indicator_moves = go.to_indicator_actions([None], state)
+        self.assertTrue(jnp.all(lax.eq(indicator_moves, jnp.zeros_like(indicator_moves))))
 
     def test_to_indicator_actions_black_move(self):
         state = go.new_states(2)
@@ -56,7 +58,21 @@ class GoTest(unittest.TestCase):
                                                                        [False, False]]]]))))
 
     def test_to_indicator_actions_pass_and_move(self):
-        self.assertEqual(True, False)  # add assertion here
+        states = go.new_states(2, batch_size=2)
+        indicator_moves = go.to_indicator_actions([None, (0, 0)], states)
+        self.assertTrue(jnp.all(lax.eq(indicator_moves[0], jnp.zeros_like(indicator_moves[0]))))
+        self.assertEqual(jnp.all(lax.eq(indicator_moves[1], jnp.array([[[[True, False],
+                                                                         [False, False]],
+                                                                        [[False, False],
+                                                                         [False, False]],
+                                                                        [[False, False],
+                                                                         [False, False]],
+                                                                        [[False, False],
+                                                                         [False, False]],
+                                                                        [[False, False],
+                                                                         [False, False]],
+                                                                        [[False, False],
+                                                                         [False, False]]]]))))
 
     def test_get_turns(self):
         states = go.new_states(2, batch_size=2)
@@ -69,13 +85,30 @@ class GoTest(unittest.TestCase):
         self.assertTrue(jnp.all(state[0, 2]))
 
     def test_black_and_white_moves_in_batch(self):
-        self.assertEqual(True, False)  # add assertion here
+        states = go.new_states(2, batch_size=2)
+        states = states.at[0, 2].set(True)
+        self.assertEqual(go.get_turns(states), [True, False])
+        states = go.next_states(states, go.to_indicator_actions([None, None], states))
+        self.assertEqual(go.get_turns(states), [False, True])
 
-    def test_pass(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_pass_changes_turn(self):
+        state = go.new_states(2)
+        self.assertEqual(go.get_turns(state), [False])
+        state = go.next_states(state, go.to_indicator_actions([None], state))
+        self.assertEqual(go.get_turns(state), [True])
+
+    def test_pass_sets_pass_layer(self):
+        state = go.new_states(2)
+        state = go.next_states(state, go.to_indicator_actions([None], state))
+        self.assertTrue(jnp.all(lax.eq(state[4]), jnp.ones_like(state[4])))
 
     def test_two_consecutive_passes_ends_game(self):
-        self.assertEqual(True, False)  # add assertion here
+        state = go.new_states(2)
+        self.assertTrue(jnp.all(lax.eq(state[5]), jnp.zeros_like(state[5])))
+        state = go.next_states(state, go.to_indicator_actions([None], state))
+        self.assertTrue(jnp.all(lax.eq(state[5]), jnp.zeros_like(state[5])))
+        state = go.next_states(state, go.to_indicator_actions([None], state))
+        self.assertTrue(jnp.all(lax.eq(state[5]), jnp.ones_like(state[5])))
 
     def test_invalid_move_space_occupied_by_opponent_pieces(self):
         state = go.new_states(2)

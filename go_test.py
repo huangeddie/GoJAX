e78a7bcdc069d0ec26b618293479992dd82e665e
Eddie Huang <aigagror@gmail.com>
2022-04-07 18:41:34 -0700

    Implemented more piece removal unit tests

--- go_test.py
+++ go_test.py
@@ -279,14 +279,50 @@ class GoTest(unittest.TestCase):
         self.assertFalse(next_state[0, gc.WHITE_CHANNEL_INDEX, 0, 0])
 
     def test_remove_two_connected_pieces(self):
-        self.assertEqual(True, False)  # add assertion here
+        state_str = """
+                            W W _ _
+                            B B _ _
+                            _ _ _ _
+                            _ _ _ _
+                            """
+        state = go.decode_state(state_str, gc.BLACKS_TURN)
+        next_state = go.next_states(state, go.to_indicator_actions([(0, 2)], state))
+        self.assertTrue(jnp.alltrue(~next_state[0, gc.WHITE_CHANNEL_INDEX]))
+        self.assertTrue(jnp.alltrue(next_state[0, gc.BLACK_CHANNEL_INDEX] == jnp.array([[False, False, True, False],
+                                                                                        [True, True, False, False],
+                                                                                        [False, False, False, False],
+                                                                                        [False, False, False, False]])),
+                        next_state[0, gc.BLACK_CHANNEL_INDEX])
 
     def test_remove_two_disjoint_pieces(self):
-        self.assertEqual(True, False)  # add assertion here
+        state_str = """
+                    W _ W B
+                    B _ B _
+                    _ _ _ _
+                    _ _ _ _
+                    """
+        state = go.decode_state(state_str, gc.BLACKS_TURN)
+        next_state = go.next_states(state, go.to_indicator_actions([(0, 1)], state))
+        self.assertTrue(jnp.alltrue(~next_state[0, gc.WHITE_CHANNEL_INDEX]))
+        self.assertTrue(jnp.alltrue(next_state[0, gc.BLACK_CHANNEL_INDEX] == jnp.array([[False, True, False, True],
+                                                                                        [True, False, True, False],
+                                                                                        [False, False, False, False],
+                                                                                        [False, False, False, False]])))
 
     def test_remove_donut(self):
-        self.assertEqual(True, False)  # add assertion here
+        state_str = """
+                            B B B W
+                            B _ B W
+                            B B B W
+                            W W W _
+                            """
+        state = go.decode_state(state_str, gc.WHITES_TURN)
+        next_state = go.next_states(state, go.to_indicator_actions([(1, 1)], state))
+        self.assertTrue(jnp.alltrue(~next_state[0, gc.BLACK_CHANNEL_INDEX]))
+        self.assertTrue(jnp.alltrue(next_state[0, gc.WHITE_CHANNEL_INDEX] == jnp.array([[False, False, False, True],
+                                                                                        [False, True, False, True],
+                                                                                        [False, False, False, True],
+                                                                                        [True, True, True, False]])))
 
-
-if __name__ == '__main__':
-    unittest.main()
+    if __name__ == '__main__':
+        unittest.main()

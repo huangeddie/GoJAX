3945ebc3b5004414977b7175daba43a6801707d7
Eddie Huang <aigagror@gmail.com>
2022-06-20 19:23:11 -0700

    Removed uniform variations of sample functions to remove initialization of arrays in functions

--- gojax/rng.py
+++ gojax/rng.py
@@ -4,7 +4,7 @@ from jax import numpy as jnp, lax
 import gojax
 
 
-def sample_actions_from_logits(states, logits, rng_key):
+def sample_actions(states, logits, rng_key):
     """
     Samples the valid actions from the logits with probability equal to softmax(
     raw_action_logits).
@@ -25,37 +25,32 @@ def sample_actions_from_logits(states, logits, rng_key):
     return actions
 
 
-def sample_actions_uniformly(states, rng_key):
+def sample_next_states(step, states, logits, rng_key):
     """
-    Samples the valid actions uniformly (see sample_actions_from_logits).
-    """
-    raw_action_logits = jnp.zeros((len(states), gojax.get_action_size(states)))
-    return sample_actions_from_logits(states, raw_action_logits, rng_key)
-
-
-def sample_next_states_uniformly(step, states, rng_key):
-    """
-    Samples the next state uniformly with RNG created by folding rng_key with step.
+    Samples the next state with probability softmax(logits) and RNG created by folding rng_key
+    with step.
 
     :param step: an integer.
     :param states: a batch array of N Go games.
+    :param logits: an N x A float array of logits.
     :param rng_key: JAX RNG key.
     :return: a batch array of N Go games.
     """
     return gojax.next_states(states,
-                             sample_actions_uniformly(states, jax.random.fold_in(rng_key, step)))
+                             sample_actions(states, logits, jax.random.fold_in(rng_key, step)))
 
 
-def sample_random_state_uniformly(board_size, batch_size, num_steps, rng_key):
+def sample_random_state(board_size, batch_size, num_steps, logits, rng_key):
     """
     Samples a random state by with `num_steps` sequential uniform random actions.
 
-    :param board_size: board size (integer).
-    :param batch_size: batch size (integer).
+    :param board_size: board size B (integer).
+    :param batch_size: batch size N (integer).
+    :param logits: an N x A float array of logits.
     :param num_steps: number of steps to take (integer).
     :param rng_key: JAX RNG key.
     :return: the final state of the game (trajectory).
     """
     return lax.fori_loop(0, num_steps,
-                         jax.tree_util.Partial(sample_next_states_uniformly, rng_key=rng_key),
+                         jax.tree_util.Partial(sample_next_states, logits=logits, rng_key=rng_key),
                          gojax.new_states(board_size, batch_size))
--- tests/test_rng.py
+++ tests/test_rng.py
@@ -12,10 +12,11 @@ import serialize
 
 class RNGTestCase(chex.TestCase):
     def test_sample_actions_from_logits(self):
-        sampled_actions = rng.sample_actions_from_logits(
-            gojax.new_states(board_size=3, batch_size=2),
-            logits=jnp.array([[10, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 10, 0, 0, 0, 0, 0, 0, 0, 0]],
-                             dtype=float), rng_key=jax.random.PRNGKey(42))
+        sampled_actions = rng.sample_actions(gojax.new_states(board_size=3, batch_size=2),
+                                             logits=jnp.array([[10, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                                               [0, 10, 0, 0, 0, 0, 0, 0, 0, 0]],
+                                                              dtype=float),
+                                             rng_key=jax.random.PRNGKey(42))
         np.testing.assert_array_equal(sampled_actions, [[[1, 0, 0], [0, 0, 0], [0, 0, 0]],
                                                         [[0, 1, 0], [0, 0, 0], [0, 0, 0]]])
         chex.assert_type(sampled_actions, bool)
@@ -27,9 +28,8 @@ class RNGTestCase(chex.TestCase):
                                     B B _
                                     TURN=W
                                     """)
-        sampled_actions = rng.sample_actions_from_logits(states,
-                                                         logits=jnp.zeros((1, 10), dtype=float),
-                                                         rng_key=jax.random.PRNGKey(10))
+        sampled_actions = rng.sample_actions(states, logits=jnp.zeros((1, 10), dtype=float),
+                                             rng_key=jax.random.PRNGKey(10))
         np.testing.assert_array_equal(sampled_actions, [[[0, 0, 0], [0, 0, 0], [0, 0, 1]]])
 
     def test_pass_from_state_with_one_piece_action(self):
@@ -39,27 +39,44 @@ class RNGTestCase(chex.TestCase):
                                     B B _
                                     TURN=W
                                     """)
-        sampled_actions = rng.sample_actions_from_logits(states,
-                                                         logits=jnp.zeros((1, 10), dtype=float),
-                                                         rng_key=jax.random.PRNGKey(42))
+        sampled_actions = rng.sample_actions(states, logits=jnp.zeros((1, 10), dtype=float),
+                                             rng_key=jax.random.PRNGKey(42))
         np.testing.assert_array_equal(sampled_actions, [[[0, 0, 0], [0, 0, 0], [0, 0, 0]]])
 
-    def test_sample_actions_uniformly(self):
+    def test_sample_actions(self):
         states = serialize.decode_states("""
                                     W W W
                                     W W _
                                     W W _
                                     """)
-        sampled_actions = rng.sample_actions_uniformly(states, jax.random.PRNGKey(10))
+        sampled_actions = rng.sample_actions(states, logits=jnp.zeros((1, 10)),
+                                             rng_key=jax.random.PRNGKey(10))
         np.testing.assert_array_equal(sampled_actions, [[[0, 0, 0], [0, 0, 0], [0, 0, 1]]])
 
-    def test_sample_random_state_uniformly(self):
-        state = rng.sample_random_state_uniformly(board_size=3, batch_size=1, num_steps=4,
-                                                  rng_key=jax.random.PRNGKey(42))
+    def test_sample_random_state(self):
+        state = rng.sample_random_state(board_size=3, batch_size=1, num_steps=4,
+                                        logits=jnp.zeros((1, 10)), rng_key=jax.random.PRNGKey(42))
         np.testing.assert_array_equal(state, serialize.decode_states("""
                                                                 W B _
                                                                 _ _ _
                                                                 _ W B
+                                                                """))
+
+    def test_sample_random_state(self):
+        state = rng.sample_random_state(board_size=9, batch_size=1, num_steps=162,
+                                        logits=jnp.zeros((1, 82)), rng_key=jax.random.PRNGKey(42))
+        print(gojax.get_pretty_string(state[0]))
+        np.testing.assert_array_equal(state, serialize.decode_states("""
+                                                                W W W B _ W W B B
+                                                                W W _ _ W W B W _
+                                                                _ W W W W B B _ W
+                                                                W _ W W B B B W W
+                                                                _ B W W B B B B B
+                                                                B _ W B B B B B B
+                                                                _ B W B B B W B B
+                                                                _ W W B B B W W W
+                                                                _ _ W W _ W W W W
+                                                                END=T;PASS=T
                                                                 """))
 
 

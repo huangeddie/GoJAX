b864f4d05b26aa4e1bcc09737a21b8665cbb288a
Eddie Huang <aigagror@gmail.com>
2022-06-20 19:37:33 -0700

    Change cardinally connected kernel from function to global constant

--- gojax/constants.py
+++ gojax/constants.py
@@ -1,5 +1,7 @@
 """Go constants."""
 
+import jax.numpy as jnp
+
 BLACKS_TURN = False
 WHITES_TURN = True
 
@@ -11,3 +13,9 @@ PASS_CHANNEL_INDEX = 4
 END_CHANNEL_INDEX = 5
 
 NUM_CHANNELS = 6
+
+# A kernel (OIHW format) used to in convolution used to expand a batch of 2D boolean arrays in
+# all four cardinal directions. Temporarily returns a float array until CUDNN can support
+# convolutions with booleans or integers.
+CARDINALLY_CONNECTED_KERNEL = jnp.array([[[[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]]]],
+                                        dtype='bfloat16')
--- gojax/go.py
+++ gojax/go.py
@@ -97,10 +97,9 @@ def _paint_fill(seeds, areas):
     :param areas: an N x 1 x B x B boolean array where the True entries are areas.
     :return: an N x 1 x B x B boolean array.
     """
-    kernel = _get_cardinally_connected_kernel()
     second_expansion = jnp.logical_and(
-        lax.conv(seeds.astype('bfloat16'), kernel, window_strides=(1, 1), padding='same').astype(
-            bool), areas)
+        lax.conv(seeds.astype('bfloat16'), constants.CARDINALLY_CONNECTED_KERNEL,
+                 window_strides=(1, 1), padding='same').astype(bool), areas)
     last_two_expansions = jnp.stack([seeds, second_expansion], axis=0)
 
     def _last_expansion_no_change(last_two_expansions_):
@@ -110,23 +109,11 @@ def _paint_fill(seeds, areas):
         last_two_expansions_ = last_two_expansions_.at[0].set(
             last_two_expansions_[1])  # Copy the second state to the first state.
         return last_two_expansions_.at[1].set(jnp.logical_and(
-            lax.conv(last_two_expansions_[1].astype('bfloat16'), kernel, window_strides=(1, 1),
+            lax.conv(last_two_expansions_[1].astype('bfloat16'),
+                     constants.CARDINALLY_CONNECTED_KERNEL, window_strides=(1, 1),
                      padding='same').astype(bool), areas))
 
     return lax.while_loop(_last_expansion_no_change, _expand, last_two_expansions)[1]
-
-
-def _get_cardinally_connected_kernel():
-    """
-    Returns a kernel (OIHW format) used to in convolution used to expand a batch of 2D boolean
-    arrays in all four cardinal directions.
-
-    This function temporarily returns a float array until CUDNN can support convolutions with
-    booleans or integers.
-
-    :return: a 1 x 1 x 3 x 3 float array.
-    """
-    return jnp.array([[[[0., 1., 0.], [1., 1., 1.], [0., 1., 0.]]]], dtype='bfloat16')
 
 
 def compute_free_groups(states, turns):
@@ -142,7 +129,7 @@ def compute_free_groups(states, turns):
     pieces = jnp.expand_dims(state_info.get_pieces_per_turn(states, turns), 1)
     empty_spaces = state_info.get_empty_spaces(states, keepdims=True)  # N x 1 x B x B array.
     immediate_free_pieces = jnp.logical_and(
-        lax.conv(empty_spaces.astype('bfloat16'), _get_cardinally_connected_kernel(), (1, 1),
+        lax.conv(empty_spaces.astype('bfloat16'), constants.CARDINALLY_CONNECTED_KERNEL, (1, 1),
                  padding='same'), pieces)
 
     return jnp.squeeze(_paint_fill(immediate_free_pieces, pieces), 1)
@@ -164,15 +151,14 @@ def compute_areas(states):
     """
     black_pieces = states[:, constants.BLACK_CHANNEL_INDEX]
     white_pieces = states[:, constants.WHITE_CHANNEL_INDEX]
-    kernel = _get_cardinally_connected_kernel()
     empty_spaces = state_info.get_empty_spaces(states, keepdims=True)
 
     immediately_connected_to_black_pieces = jnp.logical_and(
-        lax.conv(jnp.expand_dims(black_pieces, 1).astype('bfloat16'), kernel, (1, 1),
-                 padding="same"), empty_spaces)
+        lax.conv(jnp.expand_dims(black_pieces, 1).astype('bfloat16'),
+                 constants.CARDINALLY_CONNECTED_KERNEL, (1, 1), padding="same"), empty_spaces)
     immediately_connected_to_white_pieces = jnp.logical_and(
-        lax.conv(jnp.expand_dims(white_pieces, 1).astype('bfloat16'), kernel, (1, 1),
-                 padding="same"), empty_spaces)
+        lax.conv(jnp.expand_dims(white_pieces, 1).astype('bfloat16'),
+                 constants.CARDINALLY_CONNECTED_KERNEL, (1, 1), padding="same"), empty_spaces)
     connected_to_black_pieces = _paint_fill(immediately_connected_to_black_pieces, empty_spaces)
     connected_to_white_pieces = _paint_fill(immediately_connected_to_white_pieces, empty_spaces)
 

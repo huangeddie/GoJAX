415a598d571d67e9ff9248a2e25076f4ef6bdfe2
Eddie Huang <aigagror@gmail.com>
2022-06-25 19:03:32 -0700

    Add sample_non_occupied_actions and made it the default sampling function

--- gojax/rng.py
+++ gojax/rng.py
@@ -6,7 +6,7 @@ import gojax
 
 def sample_all_actions(states, logits, rng_key):
     """
-    Samples the valid actions from the logits with probability equal to softmax(
+    Samples the all actions from the logits with probability equal to softmax(
     raw_action_logits).
 
     :param states: a batch array of N Go games.
@@ -14,39 +14,35 @@ def sample_all_actions(states, logits, rng_key):
     :param rng_key: JAX RNG key.
     :return: an N x B x B boolean one-hot array representing the sampled action (all-false = pass).
     """
-    flattened_invalids = jnp.reshape(gojax.get_killed(states),
-                                     (-1, states.shape[2] * states.shape[3]))
-    action_logits = jnp.where(
-        jnp.append(flattened_invalids, jnp.zeros((len(states), 1), dtype=bool), axis=1),
-        jnp.full_like(logits, float('-inf')), logits)
-    action_1d = jax.random.categorical(rng_key, action_logits)
-    one_hot_action_1d = jax.nn.one_hot(action_1d, action_logits.shape[1], dtype=bool)
-    actions = jnp.reshape(one_hot_action_1d[:, :-1], (-1, states.shape[2], states.shape[3]))
-    return actions
+    action_1d = jax.random.categorical(rng_key, logits)
+    one_hot_action_1d = jax.nn.one_hot(action_1d, logits.shape[1], dtype=bool)
+    return jnp.reshape(one_hot_action_1d[:, :-1], (-1, states.shape[2], states.shape[3]))
 
 
 def sample_non_occupied_actions(states, logits, rng_key):
     """
-    Samples the valid actions from the logits with probability equal to softmax(
+    Samples actions that are not currently occupied from the logits with probability equal to
+    softmax(
     raw_action_logits).
+
+    Ignores corresponding occupied space logits.
 
     :param states: a batch array of N Go games.
     :param logits: an N x A float array of logits.
     :param rng_key: JAX RNG key.
     :return: an N x B x B boolean one-hot array representing the sampled action (all-false = pass).
     """
-    flattened_invalids = jnp.reshape(gojax.get_killed(states),
+    flattened_occupied = jnp.reshape(gojax.get_occupied_spaces(states),
                                      (-1, states.shape[2] * states.shape[3]))
     action_logits = jnp.where(
-        jnp.append(flattened_invalids, jnp.zeros((len(states), 1), dtype=bool), axis=1),
+        jnp.append(flattened_occupied, jnp.zeros((len(states), 1), dtype=bool), axis=1),
         jnp.full_like(logits, float('-inf')), logits)
     action_1d = jax.random.categorical(rng_key, action_logits)
     one_hot_action_1d = jax.nn.one_hot(action_1d, action_logits.shape[1], dtype=bool)
-    actions = jnp.reshape(one_hot_action_1d[:, :-1], (-1, states.shape[2], states.shape[3]))
-    return actions
+    return jnp.reshape(one_hot_action_1d[:, :-1], (-1, states.shape[2], states.shape[3]))
 
 
-def sample_next_states(step, states, logits, rng_key):
+def sample_next_states(step, states, logits, rng_key, sampling_fn=sample_non_occupied_actions):
     """
     Samples the next state with probability softmax(logits) and RNG created by folding rng_key
     with step.
@@ -55,13 +51,14 @@ def sample_next_states(step, states, logits, rng_key):
     :param states: a batch array of N Go games.
     :param logits: an N x A float array of logits.
     :param rng_key: JAX RNG key.
+    :param sampling_fn: sampling function. Either sample_all_actions or sample_non_occupied_actions.
     :return: a batch array of N Go games.
     """
-    return gojax.next_states(states,
-                             sample_all_actions(states, logits, jax.random.fold_in(rng_key, step)))
+    return gojax.next_states(states, sampling_fn(states, logits, jax.random.fold_in(rng_key, step)))
 
 
-def sample_random_state(board_size, batch_size, num_steps, logits, rng_key):
+def sample_random_state(board_size, batch_size, num_steps, logits, rng_key,
+                        sampling_fn=sample_non_occupied_actions):
     """
     Samples a random state by with `num_steps` sequential uniform random actions.
 
@@ -70,8 +67,10 @@ def sample_random_state(board_size, batch_size, num_steps, logits, rng_key):
     :param logits: an N x A float array of logits.
     :param num_steps: number of steps to take (integer).
     :param rng_key: JAX RNG key.
+    :param sampling_fn: sampling function. Either sample_all_actions or sample_non_occupied_actions.
     :return: the final state of the game (trajectory).
     """
     return lax.fori_loop(0, num_steps,
-                         jax.tree_util.Partial(sample_next_states, logits=logits, rng_key=rng_key),
+                         jax.tree_util.Partial(sample_next_states, logits=logits, rng_key=rng_key,
+                                               sampling_fn=sampling_fn),
                          gojax.new_states(board_size, batch_size))
--- tests/test_rng.py
+++ tests/test_rng.py
@@ -11,7 +11,7 @@ import serialize
 
 
 class RNGTestCase(chex.TestCase):
-    def test_sample_actions_from_low_entropy_logits(self):
+    def test_sample_all_actions_from_low_entropy_logits(self):
         sampled_actions = rng.sample_all_actions(gojax.new_states(board_size=3, batch_size=2),
                                                  logits=jnp.array([[10, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                                                                    [0, 10, 0, 0, 0, 0, 0, 0, 0, 0]],
@@ -19,6 +19,22 @@ class RNGTestCase(chex.TestCase):
                                                  rng_key=jax.random.PRNGKey(42))
         np.testing.assert_array_equal(sampled_actions, [[[1, 0, 0], [0, 0, 0], [0, 0, 0]],
                                                         [[0, 1, 0], [0, 0, 0], [0, 0, 0]]])
+        chex.assert_type(sampled_actions, bool)
+
+    def test_sample_non_occupied_actions_from_high_entropy_logits(self):
+        sampled_actions = rng.sample_non_occupied_actions(gojax.decode_states("""
+                                                                    B B B
+                                                                    B B B
+                                                                    B B _
+                                                                    
+                                                                    _ B B
+                                                                    B B B
+                                                                    B B B
+                                                                    """), logits=jnp.array(
+            [[10, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 10, 0, 0, 0, 0, 0, 0, 0, 0]], dtype=float),
+                                                          rng_key=jax.random.PRNGKey(1))
+        np.testing.assert_array_equal(sampled_actions, [[[0, 0, 0], [0, 0, 0], [0, 0, 1]],
+                                                        [[0, 0, 0], [0, 0, 0], [0, 0, 0]]])
         chex.assert_type(sampled_actions, bool)
 
     def test_sample_from_zero_logits(self):
@@ -31,10 +47,9 @@ class RNGTestCase(chex.TestCase):
         state = rng.sample_random_state(board_size=3, batch_size=1, num_steps=4,
                                         logits=jnp.zeros((1, 10)), rng_key=jax.random.PRNGKey(42))
         np.testing.assert_array_equal(state, serialize.decode_states("""
+                                                                W B _
                                                                 _ _ _
-                                                                _ _ _
-                                                                _ B B
-                                                                PASS=T
+                                                                _ W B
                                                                 """))
 
 

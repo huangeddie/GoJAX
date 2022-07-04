13a782577d644128dd70f474503c85313d18d1e6
Eddie Huang <aigagror@gmail.com>
2022-07-04 12:50:12 -0700

    Expand thrice

--- gojax/go.py
+++ gojax/go.py
@@ -49,6 +49,9 @@ def paint_fill(seeds, areas):
         expanded = lax.min(
             lax.conv(expanded, constants.CARDINALLY_CONNECTED_KERNEL, window_strides=(1, 1),
                      padding='same'), float_areas)
+        expanded = lax.min(
+            lax.conv(expanded, constants.CARDINALLY_CONNECTED_KERNEL, window_strides=(1, 1),
+                     padding='same'), float_areas)
         return last_two_expansions_[1], expanded
 
     return lax.while_loop(_last_expansion_no_change, _expand_some, (float_seeds, second_expansion))[

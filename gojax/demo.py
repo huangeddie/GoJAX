9c84feed0951d361a16fe324a9e6f2222f4d7712
Eddie Huang <aigagror@gmail.com>
2022-11-07 21:11:57 -0800

    Fix demo.py

--- gojax/demo.py
+++ gojax/demo.py
@@ -2,21 +2,19 @@
 
 from jax import numpy as jnp
 
-import serialize
-import state_index
-from gojax import constants
-from gojax import go
+import gojax
 
 if __name__ == '__main__':
-    state = go.new_states(7)
-    while not jnp.alltrue(state[0, constants.END_CHANNEL_INDEX]):
-        print(serialize.get_string(state[0]))
+    state = gojax.new_states(7)
+    while not jnp.alltrue(state[0, gojax.END_CHANNEL_INDEX]):
+        print(gojax.get_string(state[0]))
         USER_INPUT = input('row col: ').strip()
         # pylint: disable=invalid-name
         action = None
         if USER_INPUT:
             row, col = USER_INPUT.split()
             action = (int(row), int(col))
-        state = go.next_states_legacy(state, state_index.action_2d_to_indicator([action], state))
+        state = gojax.next_states_legacy(
+            state, gojax.action_2d_to_indicator([action], state))
 
-    print(serialize.get_string(state[0]))
+    print(gojax.get_string(state[0]))
